b4114d24436ec67b332f64b943519bda111e7eb3
novasomindustries <novasomindustries@Deb9.Deb9>
2019-06-25 09:50:58 +0200

    Fixed some scripts, added parameter to user_dtb_compile for kernel use

--- FsBsp/novasis/NOVAsomM7/Init/bin/go_eth.sh
+++ FsBsp/novasis/NOVAsomM7/Init/bin/go_eth.sh
@@ -5,17 +5,19 @@
 killall udhcpc
 killall wpa_supplicant
 ifconfig ${WLAN_DEVICE} down
+sleep 1
 rmmod 8723bs
 # LED on lan connector on
 i2cset -f -y 1 0x18 0x52 0
 sleep 1
 
-Use the code below for a static ip wifi address
 if [ "${NET_USE_DHCP}" == "Y" ]; then
 	udhcpc -i ${NET_DEVICE} &
 else
 	ifconfig ${NET_DEVICE} ${NET_IP_ADDRESS} up
 	route add default gw ${NET_GATEWAY}
 fi
+# Load anyway the 8723bs module
+sleep 1
 modprobe 8723bs
 
